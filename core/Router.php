43da40a949efc1adfa4541b0c62ac8ae3c7aec7c
perejro <perejro@centro.team>
2022-09-28 12:47:35 +0300

    change path

--- core/Router.php
+++ core/Router.php
@@ -2,7 +2,7 @@
   namespace Core;
 
   class Router {
-    public string $path = '/string';
+    public string $path = '/home';
 
     public function run(): string {
       return var_export($this->path);
